fc41c36d8a4b7aa53fb349819e875859e45ae7b5
John Otander <johnotander@gmail.com>
2019-05-13 11:14:34 -0600

    Change token var, pass username as arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 FOLDER=$1
+GITHUB_USERNAME=$2
 BASE=$(pwd)
 
 # sync to read-only clones
@@ -12,7 +13,7 @@ for folder in $FOLDER/*; do
 
   # clone, delete files in the clone, and copy (new) files over
   # this handles file deletions, additions, and changes seamlessly
-  git clone --depth 1 https://$GITHUB_API_TOKEN@github.com/$GITHUB_USERNAME/$NAME.git $CLONE_DIR
+  git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$NAME.git $CLONE_DIR
   cd $CLONE_DIR
   find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
   cp -r $BASE/$folder/. .
